41dd4667f4849b6effe35bcd32dfcc9dd330d54f
Michael Louie Loria <michaellouieloria@gmail.com>
2013-11-26 13:20:17 +0800

    add collation

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -11,6 +11,7 @@ return array(
             'username' => 'modesta',
             'password' => 'ZgItHe57',
             'charset'  => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix'   => '',
         ),
     ),
