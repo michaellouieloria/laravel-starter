efa52cb68ba5b41cc9b8d116aa1abd5e9c183365
Michael Louie Loria <michaellouieloria@gmail.com>
2013-11-26 20:16:48 +0800

    home layout

--- app/views/site/home/index.blade.php
+++ app/views/site/home/index.blade.php
@@ -1,4 +1,4 @@
-@extends('site.layouts.default')
+@extends('site.layouts.home')
 
 {{-- Web site Title --}}
 @section('title')
---
+++ app/views/site/layouts/home.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Basic Page Needs
+		================================================== -->
+		<meta charset="utf-8" />
+		<title>
+			@section('title')
+			Project
+			@show
+		</title>
+        <meta name="keywords" content="@yield('meta_keywords', 'Keywords')" />
+        <meta name="author" content="@yield('meta_author', 'Author')" />
+        <!-- Google will often use this as its description of your page/site. Make it good. -->
+        <meta name="description" content="@yield('meta_description', 'Description')" />
+
+        <!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
+        <meta name="google-site-verification" content="">
+
+        <!-- Dublin Core Metadata : http://dublincore.org/ -->
+        <meta name="DC.title" content="@yield('meta_title', 'Project')">
+        <meta name="DC.subject" content="@yield('meta_description', 'Description')">
+        <meta name="DC.creator" content="@yield('meta_author', 'Author')">
+
+		<!-- Mobile Specific Metas
+		================================================== -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!-- Favicons
+		================================================== -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{{ asset('assets/ico/apple-touch-icon-144-precomposed.png') }}}">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{{ asset('assets/ico/apple-touch-icon-114-precomposed.png') }}}">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{{ asset('assets/ico/apple-touch-icon-72-precomposed.png') }}}">
+		<link rel="apple-touch-icon-precomposed" href="{{{ asset('assets/ico/apple-touch-icon-57-precomposed.png') }}}">
+		<link rel="shortcut icon" href="{{{ asset('assets/ico/favicon.png') }}}">
+        
+		<!-- CSS
+		================================================== -->
+        {{ Basset::show('public.css') }}
+
+		<style>
+        body {
+            padding: 60px 0;
+        }
+        
+		@section('styles')
+		@show
+		</style>        
+	</head>
+
+	<body>
+		<!-- To make sticky footer need to wrap in a div -->
+		<div id="wrap">
+		<!-- Navbar -->
+		<div class="navbar navbar-default navbar-fixed-top">
+			 <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+						<li {{ (Request::is('/') ? ' class="active"' : '') }}><a href="{{{ URL::to('') }}}">Home</a></li>
+                        <li {{ (Request::is('blog') ? ' class="active"' : '') }}><a href="{{{ URL::to('blog') }}}">Blog</a></li>
+                        <li {{ (Request::is('about') ? ' class="active"' : '') }}><a href="{{{ URL::to('about') }}}">About</a></li>
+                        <li {{ (Request::is('contact-us') ? ' class="active"' : '') }}><a href="{{{ URL::to('contact-us') }}}">Contact Us</a></li>
+					</ul>
+
+                    <ul class="nav navbar-nav pull-right">
+                        @if (Auth::check())
+                        @if (Auth::user()->hasRole('admin'))
+                        <li><a href="{{{ URL::to('admin') }}}">Admin Panel</a></li>
+                        @endif
+                        <li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
+                        <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
+                        @else
+                        <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
+                        <li {{ (Request::is('user/register') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
+                        @endif
+                    </ul>
+					<!-- ./ nav-collapse -->
+				</div>
+			</div>
+		</div>
+		<!-- ./ navbar -->
+
+        <!-- Content -->
+        @yield('content')
+        <!-- ./ content -->
+
+		<!-- the following div is needed to make a sticky footer -->
+		<div id="push"></div>
+		</div>
+		<!-- ./wrap -->
+
+
+	    <div id="footer">
+	      <div class="container">
+	        <p class="muted credit">Project {{ date('Y') }}</p>
+	      </div>
+	    </div>
+
+		<!-- Javascripts
+		================================================== -->
+        {{ Basset::show('public.js') }}
+	</body>
+</html>
